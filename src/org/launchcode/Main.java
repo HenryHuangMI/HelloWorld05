c16863267d2ee2fe19a7646f1f9bb0b770e7b4a2
Henry Huang <hhuang500@yahoo.com>
2019-11-30 12:33:33 -0500

    Add after connecting Github message

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello World 5");
         System.out.println("This is a change after initial commit");
+        System.out.println("This is my commit after connecting to GitHub");
     }
 }
